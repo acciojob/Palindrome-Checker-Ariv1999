b38839f753cd012617f2b10658a764d9c592745c
Sarbojit Bhattacharjee <70182981+Ariv1999@users.noreply.github.com>
2023-01-19 19:40:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 // complete the given function
-
+const text = document.getElementById('text');
+const buttom = document.getElementById('check');
+const outPut = document.getElementById('outPut');
+const str = text.value;
+buttom.setEventListener('click', palindrome);
 function palindrome(str){
 	const revString = str.split('').reverse().join('');
-	return revString === str ? true : false;
+	if(revString === str){
+		outPut.innerText = 'true';
+	}else{
+		outPut.innerText = 'false';
+	}
 }
-module.exports = palindrome('maam');
